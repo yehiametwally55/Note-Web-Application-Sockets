176c08518dee3f081e6275ceca358f14c4e054fd
Yehia Metwally <78654788+yehiametwally55@users.noreply.github.com>
2025-05-28 17:47:36 -0400

    Update main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -12,27 +12,48 @@ function addNote(){
         description: document.getElementById("notedesc").value
     }
     socket.emit("addNote", note)
-   
+    document.getElementById('noteTitle').value = "";
+    document.getElementById('notedesc').value = "";
 }
 
 socket.on("allData",(data)=>{
     console.log(data);
-    allNotes = data
+    allNotes = data;
     displayData();
 });
 
-function displayData(){
+function displayData() {
     let cartona = ``;
-    for (let i = 0; i < allNotes.length; i++) {
-        cartona += ` <div class="col-md-4 mb-5">
-        <div class="border bg-white text-dark rounded-3 p-5 text-center">
-          <h3>${allNotes[i].name}</h3>
-          <p>${allNotes[i].description}</p>
-          <button class="btn btn-danger" onclick="DeleteNote('${allNotes[i]._id}')">Delete</button>
-        </div>
-      </div>`   
+
+    if (allNotes.length === 0) {
+        cartona = `
+            <div class="col-12">
+                <div class="empty-state-message text-center">
+                    <i class="fa-regular fa-note-sticky fa-5x mb-4"></i>
+                    <h3>You have no notes yet.</h3>
+                    <p>Add your first note using the form above!</p>
+                </div>
+            </div>
+        `;
+    } else {
+        for (let i = 0; i < allNotes.length; i++) {
+            cartona += `
+            <div class="col-md-4 mb-4">
+                <div class="note-card h-100">
+                    <div class="note-header">
+                        <h3 class="note-title">${allNotes[i].name}</h3>
+                        <div class="note-actions">
+                             <button class="btn-icon" onclick="DeleteNote('${allNotes[i]._id}')">
+                               <i class="fa-solid fa-trash"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <p class="note-description">${allNotes[i].description}</p>
+                </div>
+            </div>`;
+        }
     }
-    document.getElementById("rows").innerHTML = cartona
+    document.getElementById("rows").innerHTML = cartona;
 }
 
 function DeleteNote(id){
